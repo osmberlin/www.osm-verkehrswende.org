93c70afd3614cfe1f105fefa73c7356fad7451e3
Tobias <tobias@fixmycity.de>
2023-10-21 15:28:10 +0200

    Layout: Extract prose classes

--- src/layouts/baseStyles.ts
+++ src/layouts/baseStyles.ts
@@ -1,2 +1,10 @@
+import { twJoin } from 'tailwind-merge'
+
 export const menuActiveClasses =
   'border-teal-600 bg-emerald-50 hover:bg-emerald-50 cursor-text font-semibold'
+
+export const proseBase = twJoin(
+  'prose prose-blue',
+  '[--tw - prose - bullets: _gray]',
+  'prose-code:rounded prose-code:bg-neutral-100 prose-code:box-decoration-clone prose-code:py-1 prose-code:px-1.5 prose-code:text-neutral-800 prose-code:before:content-none prose-code:after:content-none',
+)

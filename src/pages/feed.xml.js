bbba9a65c2e172425c3b3125e6fdc5968bb76a7b
Tobias <tobias@fixmycity.de>
2023-10-21 21:06:03 +0200

    Feed: Use new meta tag info; Add rss feed to Layout; cleanup unused const file

--- src/pages/feed.xml.js
+++ src/pages/feed.xml.js
@@ -1,12 +1,12 @@
 import rss from '@astrojs/rss'
 import { getCollection } from 'astro:content'
-import { SITE_TITLE, SITE_DESCRIPTION } from '../consts'
+import { projectToNavigation } from '../layouts/Navigation/projectToNavigation.const'
 
 export async function get(context) {
   const posts = await getCollection('posts')
   return rss({
-    title: SITE_TITLE,
-    description: SITE_DESCRIPTION,
+    title: projectToNavigation.about.meta.title,
+    description: projectToNavigation.about.meta.description,
     site: context.site,
     items: posts.map((post) => ({
       ...post.data,

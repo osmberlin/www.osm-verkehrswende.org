e5410af3a28e7f47b5adb300ecacda4854b00261
Tobias <tobias@fixmycity.de>
2023-10-22 21:38:58 +0200

    Extract ProjectHeadline, ProseSection Components; Rework corresponding pages; Centralize prose classes

--- src/layouts/baseStyles.ts
+++ src/layouts/baseStyles.ts
@@ -5,11 +5,21 @@ export const menuActiveClasses =
 
 export const proseBase = twJoin(
   'prose prose-blue',
-  '[--tw - prose - bullets: _gray]',
+  // <ul>
+  '[--tw-prose-bullets:_gray]',
+  // <code>
   'prose-code:rounded prose-code:bg-neutral-100 prose-code:box-decoration-clone prose-code:py-1 prose-code:px-1.5 prose-code:text-neutral-800 prose-code:before:content-none prose-code:after:content-none',
+  // <blockquote>
+  'prose-blockquote:bg-gray-100 prose-blockquote:p-4 prose-blockquote:pb-2',
 )
 
-export const proseSection = twJoin(
-  proseBase,
-  'prose-lg mb-20 prose-headings:text-sky-700 prose-headings:mb-1',
+export const proseHeadings = twJoin(
+  // <h#> anchor position
+  'prose-headings:scroll-mt-10',
+  // <h1>
+  'prose-h1:text-sky-700 prose-h1:mb-3 prose-h1:mt-0',
+  // <h2>
+  'prose-h2:mt-16 prose-h2:mb-4',
+  // <h3>
+  'prose-h3:mt-8 prose-h3:mb-4',
 )

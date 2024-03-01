9c6c2189c1b33b3a18908ff867b14e2b41c36472
Tobias <tobias@fixmycity.de>
2024-03-01 08:38:34 +0100

    Mapillary: Fix boundaries source and layer

--- src/components/page_mapillary/Map/MapSourceBoundaries.tsx
+++ src/components/page_mapillary/Map/MapSourceBoundaries.tsx
@@ -5,20 +5,20 @@ export const MapSourceBoundaries = () => {
     <Source
       id="boundaries"
       type="vector"
-      tiles={['https://tiles.radverkehrsatlas.de/public.boundaries/{z}/{x}/{y}.pbf']}
+      tiles={['https://tiles.radverkehrsatlas.de/boundaries/{z}/{x}/{y}']}
       attribution=""
     >
       <Layer
-        source="tarmac_boundaries"
-        source-layer="public.boundaries"
+        source="atlas_boundaries"
+        source-layer="boundaries"
         type="line"
         paint={{
           'line-color': 'hsl(323, 43%, 76%)',
           'line-opacity': 0.6,
           'line-dasharray': [2.5, 1, 1, 1],
-          'line-width': 1.5,
+          'line-width': 2,
         }}
-        filter={['match', ['get', 'admin_level'], ['9', '10'], true, false]}
+        filter={['match', ['get', 'admin_level'], [9, 10], true, false]}
       />
     </Source>
   )

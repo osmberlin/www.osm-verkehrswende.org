cbd33c2a2e29fd365879c550004adef6dc18bc79
Tobias <tobias@fixmycity.de>
2024-03-09 05:53:53 +0100

    CQI: Inspector: Refactor into separate file

--- src/components/project_cqi/page_map/MapInspector.tsx
+++ src/components/project_cqi/page_map/MapInspector.tsx
@@ -1,69 +1,7 @@
-import { $clickedMapData, $searchParams } from '@components/BaseMap/store'
+import { $clickedMapData } from '@components/BaseMap/store'
 import { useStore } from '@nanostores/react'
-import { twJoin } from 'tailwind-merge'
-import { legendLts } from './layers/layersLts'
-import type { SearchParamsCqiMap } from './storeCqi'
+import { MapInspectorPrimaryInformation } from './inspector/MapInspectorPrimaryInformation'
 import { skipInspectorKeys, translationTagValues, translationsKey } from './translations.const'
-import { legend1to100 } from './layers/layers1to100'
-
-function MapInspectorPrimaryIndex({ properties }: { properties: GeoJSON.Feature['properties'] }) {
-  const params = useStore($searchParams) as SearchParamsCqiMap
-  if (!properties) {
-    return null
-  }
-  if (params.anzeige === '1to100') {
-    // Der index berechnet sich aus base_index (Basisindex entsprechend des Wegetyps) * fac_1 (Faktor für Breite und Oberfläche) * fac_2 (Faktor für Straßenklasse und Höchstgeschwindigkeit) * fac_3 (Faktor für Physische Trennung und Sicherheitsabstand, derzeit nicht implementiert und daher immer 1) * fac_4 (Sonstige auf- oder abwertende Umgebungsvariablen). Die einzelnen Faktoren wiederum basieren insbesondere auf den proc_*-Werten, also fac_1 z.B. auf proc_width und proc_surface. Da steckt aber viel Vodoo und Gewichtungen dahinter, daher ist mMn eigentlich höchstens die Anzeige der fac_1..4 sinnvoll.
-    const color = legend1to100.find((l) => l.key === `index_${properties.index_10}`)?.color
-    return (
-      <div className="my-2 flex items-center justify-center gap-1">
-        {properties.index != 1 && <>1…</>}
-        <span
-          className="inline-flex min-h-12 min-w-12 items-center justify-center rounded-full bg-gray-500 text-xl font-bold leading-tight text-gray-950"
-          style={{ backgroundColor: color }}
-        >
-          {String(properties.index)}
-        </span>
-        {properties.index != 100 && <>…100</>}
-      </div>
-    )
-  }
-  if (params.anzeige === 'lts') {
-    const displayLevels = [1, 2, 3, 4]
-    return (
-      <div className="my-2 flex justify-center gap-1">
-        {displayLevels.map((displayLevel) => {
-          const current = properties.stress_level === displayLevel
-          const color = legendLts.find((l) => l.key === `stress_level_${displayLevel}`)?.color
-          return (
-            <div
-              className={twJoin(
-                'flex min-h-12 min-w-12 items-center justify-center rounded-full border-2 leading-tight',
-                current
-                  ? 'bg-gray-500 text-2xl font-bold text-gray-950'
-                  : 'bg-gray-900 text-xl text-gray-200',
-              )}
-              style={{
-                backgroundColor: current ? color : undefined,
-                borderColor: color,
-              }}
-            >
-              {String(displayLevel)}
-            </div>
-          )
-        })}
-      </div>
-    )
-  }
-  if (params.anzeige === 'incompleteness') {
-    return (
-      <div className="flex justify-center bg-white/20 p-2 text-xl">
-        {!properties.data_missing && <>Keine</>}
-        {properties.data_missing && <>{properties.data_missing}</>}
-      </div>
-    )
-  }
-  return null
-}
 
 export const MapInspector = () => {
   const clickedMapData = useStore($clickedMapData)
@@ -96,7 +34,7 @@ export const MapInspector = () => {
         return (
           <div key={feature.properties.id} className="mt-8">
             <h2 className="text-lg">{feature.properties.name || feature.properties.id}</h2>
-            <MapInspectorPrimaryIndex properties={feature.properties} />
+            <MapInspectorPrimaryInformation properties={feature.properties} />
 
             <table className="w-full">
               <tbody>

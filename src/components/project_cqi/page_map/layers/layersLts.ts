14dea891312fc067ba1d7d667856294ae0dc4a8e
Tobias <tobias@fixmycity.de>
2024-03-29 13:10:47 +0100

    CQI: Rename some filter keys

--- src/components/project_cqi/page_map/layers/layersLts.ts
+++ src/components/project_cqi/page_map/layers/layersLts.ts
@@ -147,28 +147,42 @@ export const legendLts: LegendGroup[] = [
     primary: true,
     legends: [
       {
-        key: 'stress_level_1',
+        key: 'lts1',
         color: '#4589fc',
         label: 'LTS 1 (Kindertauglich)',
         filterConfig: { key: 'stress_level', values: [1] },
       },
       {
-        key: 'stress_level_2',
+        key: 'lts2',
         color: '#a7c878',
         label: 'LTS 2 (Niedriger Stress)',
         filterConfig: { key: 'stress_level', values: [2] },
       },
       {
-        key: 'stress_level_3',
+        key: 'lts3',
         color: '#ffcf23',
         label: 'LTS 3 (Mittlerer Stress)',
         filterConfig: { key: 'stress_level', values: [3] },
       },
       {
-        key: 'stress_level_4',
+        key: 'lts4',
         color: '#ab1b04',
         label: 'LTS 4 (Kampfradler, hoher Stress)',
         filterConfig: { key: 'stress_level', values: [4] },
+      },
+    ],
+  },
+  {
+    title: 'Nutzbarkeit',
+    key: 'usable',
+    primary: false,
+    legends: [
+      {
+        key: 'yes',
+        color: 'gray',
+        label: 'Nur nutzbare anzeigen',
+        filterConfig: { key: 'filter_usable', values: [1] },
+        defaultOn: true,
       },
     ],
   },

9c41f6c203242f2ec03049a71a977b8acdd558b7
Tobias <tobias@fixmycity.de>
2024-09-06 14:03:05 +0200

    Netlify: Change env var import in keystatic config
    Which might resolve our build issue https://github.com/withastro/astro/issues/10007#issuecomment-1932161304

--- keystatic.config.ts
+++ keystatic.config.ts
@@ -2,15 +2,10 @@ import { config } from '@keystatic/core'
 import { keystaticPostsConfig } from 'keystatic/keystatic.posts.config'
 import { keystaticProjectsConfig } from 'keystatic/keystatic.projects.config'
 
-// See ./astro.config.mjs
-import { loadEnv } from 'vite'
-const { ASTRO_STORAGE_KIND } = loadEnv(process.env.NODE_ENV!, process.cwd(), '')
-
 export default config({
   storage: {
     // https://keystatic.com/docs/github-mode#setting-up-git-hub-mode
-    // @ts-expect-error
-    kind: ASTRO_STORAGE_KIND,
+    kind: import.meta.env.PUBLIC_ASTRO_STORAGE_KIND,
     repo: {
       owner: 'osmberlin',
       name: 'www.osm-verkehrswende.org',
--- src/env.d.ts
+++ src/env.d.ts
@@ -7,7 +7,7 @@ declare module '*.astro'
 interface ImportMetaEnv {
   readonly ASTRO_OUTPUT_MODE: 'hybrid' | 'static'
   readonly ASTRO_USE_NETLIFY_ADAPTER: 'true' | 'false'
-  readonly ASTRO_STORAGE_KIND: 'local' | 'github'
+  readonly PUBLIC_ASTRO_STORAGE_KIND: 'local' | 'github'
 }
 
 interface ImportMeta {

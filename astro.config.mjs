2fef60c81a197ba808c800e4bdcd5fcead548b5d
Tobias <tobias@fixmycity.de>
2024-09-06 11:41:04 +0200

    Update configs to work locally, on Netlify and on Github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,9 +8,29 @@ import path from 'path'
 import remarkToc from 'remark-toc'
 import { watchAndRun } from 'vite-plugin-watch-and-run'
 
+// ABOUT:
+// We have to fetch settings from `.env`
+// Which we have to do manually, see https://docs.astro.build/en/guides/configuring-astro/#environment-variables
+//
+// USAGE:
+// `npm run dev` uses hybrid mode and keystatic()
+// `npm run build` (server) is based on .env and has different settings for Netlify (CMS/Keystatic) vs. Github Pages (Static site)
+// `npm run build:local && npm run serve` overwrites the .env settings to have a local test case for what is on Github Pages
+import { loadEnv } from 'vite'
+const { ASTRO_OUTPUT_MODE, ASTRO_USE_NETLIFY_ADAPTER } = loadEnv(
+  process.env.NODE_ENV,
+  process.cwd(),
+  '',
+)
+
+// CONFIG:
 // https://astro.build/config
 export default defineConfig({
-  output: 'hybrid',
+  // On Netlify and during development we use `hybrid`, on Github Pages we usd `static`.
+  // Using static makes sure features like Astros redirecting work as expected.
+  // Docs https://docs.astro.build/en/basics/rendering-modes/
+  output: ASTRO_OUTPUT_MODE,
+  // adapter: ASTRO_USE_NETLIFY_ADAPTER === 'true' ? netlify() : undefined,
   integrations: [
     tailwind({
       // https://github.com/withastro/astro/tree/main/packages/integrations/tailwind#applybasestyles
@@ -21,7 +41,7 @@ export default defineConfig({
     sitemap({
       filter: (page) => !page.endsWith('README/'),
     }),
-    keystatic(),
+    ASTRO_OUTPUT_MODE === 'hybrid' ? keystatic() : undefined,
   ],
   markdown: {
     remarkPlugins: [remarkToc],
@@ -36,7 +56,11 @@ export default defineConfig({
       noExternal: ['route-snapper'],
     },
     optimizeDeps: {
-      exclude: ['route-snapper'],
+      exclude: [
+        'route-snapper',
+        // Needed for `watchAndRun` https://stackoverflow.com/a/75655669
+        'fsevents',
+      ],
     },
     plugins: [
       // See keystatic/scripts/README.md
--- src/env.d.ts
+++ src/env.d.ts
@@ -3,3 +3,13 @@
 
 // Get rid of missleading warning when running `tsc`.
 declare module '*.astro'
+
+interface ImportMetaEnv {
+  readonly ASTRO_OUTPUT_MODE: 'hybrid' | 'static'
+  readonly ASTRO_USE_NETLIFY_ADAPTER: 'true' | 'false'
+  readonly ASTRO_STORAGE_KIND: 'local' | 'github'
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}

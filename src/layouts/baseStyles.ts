ecf4f599ac07704a019e23fd21f297d814a9df51
Tobias <tobias@fixmycity.de>
2024-09-06 16:12:52 +0200

    Autoformatting

--- src/layouts/baseStyles.ts
+++ src/layouts/baseStyles.ts
@@ -8,7 +8,7 @@ export const proseBase = twJoin(
   // <ul>
   '[--tw-prose-bullets:_gray]',
   // <code>
-  'prose-code:rounded prose-code:bg-neutral-100 prose-code:box-decoration-clone prose-code:py-1 prose-code:px-1.5 prose-code:text-neutral-800 prose-code:before:content-none prose-code:after:content-none',
+  'prose-code:rounded prose-code:bg-neutral-100 prose-code:box-decoration-clone prose-code:px-1.5 prose-code:py-1 prose-code:text-neutral-800 prose-code:before:content-none prose-code:after:content-none',
   // <blockquote>
   'prose-blockquote:bg-gray-100 prose-blockquote:p-4 prose-blockquote:pb-2',
 )
@@ -17,9 +17,9 @@ export const proseHeadings = twJoin(
   // <h#> anchor position
   'prose-headings:scroll-mt-10',
   // <h1>
-  'prose-h1:text-sky-700 prose-h1:mt-16 prose-h1:mb-3 first:prose-h1:mt-0',
+  'prose-h1:mb-3 prose-h1:mt-16 prose-h1:text-sky-700 first:prose-h1:mt-0',
   // <h2>
-  'prose-h2:mt-16 prose-h2:mb-4',
+  'prose-h2:mb-4 prose-h2:mt-16',
   // <h3>
-  'prose-h3:mt-8 prose-h3:mb-4',
+  'prose-h3:mb-4 prose-h3:mt-8',
 )
